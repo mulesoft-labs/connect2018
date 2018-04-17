258836ea3ebd4279fad83487eb70613c519f8fc3
renancamponez <rcamponez@avenuecode.com>
2018-04-17 14:29:25 -0700

    Initial revision

---
+++ social-recommendations-app/src/main/java/com/mulesoft/social/Choices.java
@@ -0,0 +1,23 @@
+package com.mulesoft.social;
+
+import java.util.ArrayList;
+
+public class Choices {
+	ArrayList<String> artPieces = new ArrayList();
+	ArrayList<String> paintColors = new ArrayList();
+	
+	public ArrayList<String> getArtPieces() {
+		return artPieces;
+	}
+	public void setArtPieces(ArrayList<String> artPieces) {
+		this.artPieces = artPieces;
+	}
+	public ArrayList<String> getPaintColors() {
+		return paintColors;
+	}
+	public void setPaintColors(ArrayList<String> paintColors) {
+		this.paintColors = paintColors;
+	}
+	
+	
+}
